e7f5384c9d02cbc058f9d1c92d8b9f8013a5e712
Vincent Corbee <vincentcorbee@Vincents-MacBook-Pro-5052.local>
2025-05-25 19:57:22 +0200

    feat: part three added memory controller

--- src/instructions/data-processing/logical/mov.ts
+++
@@ -1,6 +0,0 @@
-import { MOV } from "../../../constants/codes"
-import { dataProcessing, DataProcessingArgs } from "../data-processing"
-
-export type MovArgs = Pick<DataProcessingArgs, 'cond' | 'rd' | 'i' | 'operand2' | 's'>
-
-export const mov = (args: MovArgs) => dataProcessing({ ...args, opCode: MOV });
--- src/instructions/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './data-processing'
-export * from './software-interrupt'
-export * from './branch'
-export * from './single-data-transfer'
-export * from './block-data-transfer'
-export * from './multiply'
